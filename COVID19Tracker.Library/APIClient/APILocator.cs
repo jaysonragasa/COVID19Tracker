ee72b9d5eff958a61f764504787bcf372e9d07e2
Jayson Ragasa <jayson.d.ragasa@live.com>
2020-04-25 19:29:28 +0800

    use CT DS

--- COVID19Tracker.Library/APIClient/APILocator.cs
+++ COVID19Tracker.Library/APIClient/APILocator.cs
@@ -1,7 +1,7 @@
 ﻿// Data sources
 
-using COVID19Tracker.Library.APIClient.DataSources.Demo;
-//using COVID19Tracker.Library.APIClient.DataSources.CoronaTracker;
+//using COVID19Tracker.Library.APIClient.DataSources.Demo;
+using COVID19Tracker.Library.APIClient.DataSources.CoronaTracker;
 //using COVID19Tracker.Library.APIClient.DataSources.MyGitRepo;
 
 namespace covid19phlib.APIClient
